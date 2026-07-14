4ce1fae57a7d8b2230dc42877665c6c9008ab101
Francesco Neri <francesconeri95@gmail.com>
2026-07-14 17:23:11 +0200

    update testcase

--- testcases/mesh_refinement/main.py
+++ testcases/mesh_refinement/main.py
@@ -1,7 +1,11 @@
 from pyshockflow import Driver
 from pyshockflow import Config
 
-ins = ['input_normal.ini', 'input_refined.ini']
+ins = [
+    'input_normal.ini', 
+    'input_refined.ini',
+    'input_refined_adaptive.ini',
+    ]
 
 for input in ins:
     config = Config(input)
--- testcases/mesh_refinement/post_process.py
+++ testcases/mesh_refinement/post_process.py
@@ -8,11 +8,27 @@ from pyshockflow.fluid import FluidIdeal
 
 fluid = FluidIdeal(1.4, 287.05)
 
+refinement_locations = [0.4, 0.6]
 
-resultsFile = ["Results/normal_NX_500/Results.pik",
-               "Results/refined_NX_498/Results.pik"]
+resultsFile = [
+    "Results/normal_NX_500/Results.pik",
+    "Results/refined_NX_500/Results.pik",
+    "Results/refined_adaptive_NX_504/Results.pik"
+    ]
 
-labels = ['Normal', 'Local Refinement']
+labels = [
+          'Normal', 
+          'Refined', 
+          'Refined Adaptive',
+          ]
+
+lss = [
+    '-',
+    '--',
+    '-.',
+]
+
+lw = 2.5
 
 outFolder = 'Pictures'
 os.makedirs(outFolder, exist_ok=True)
@@ -22,35 +38,35 @@ for i in range(len(resultsFile)):
     with open(resultsFile[i], 'rb') as file:
         resultsPickle.append(pickle.load(file))
         
+time = resultsPickle[0]['Time']
+ntimes = len(time)    
+        
 fig, ax = plt.subplots(2, 2, figsize=(16,10))
-
+    
 for i,results in enumerate(resultsPickle):
-    ax[0,0].plot(results['X Coords'][1:-1], results['Primitive']["Density"][1:-1, -1], label=labels[i])
+    results['Primitive']["Energy"] = fluid.computeStaticEnergy_p_rho(results['Primitive']["Pressure"], results['Primitive']["Density"])
+    
+    ax[0,0].plot(results['X Coords'], results['Primitive']["Density"][:, -1], label=labels[i], ls=lss[i], linewidth=lw)
+    ax[0,1].plot(results['X Coords'], results['Primitive']["Velocity"][:, -1], label=labels[i], ls=lss[i], linewidth=lw)
+    ax[1,0].plot(results['X Coords'], results['Primitive']["Pressure"][:, -1], label=labels[i], ls=lss[i], linewidth=lw)
+    ax[1,1].plot(results['X Coords'], results['Primitive']["Energy"][:, -1], label=labels[i], ls=lss[i], linewidth=lw)
+
 ax[0, 0].set_ylabel(r'Density')
-
-for i,results in enumerate(resultsPickle):
-    ax[0,1].plot(results['X Coords'][1:-1], results['Primitive']["Velocity"][1:-1, -1], label=labels[i])
 ax[0, 1].set_ylabel(r'Velocity')
-
-for i,results in enumerate(resultsPickle):
-    ax[1,0].plot(results['X Coords'][1:-1], results['Primitive']["Pressure"][1:-1, -1], label=labels[i])
-ax[1, 0].set_ylabel(r'Pressure')
-
-
-
-for i,results in enumerate(resultsPickle):
-    results['Primitive']["Mach"] = fluid.computeMach_u_p_rho(results['Primitive']["Velocity"], results['Primitive']["Pressure"], results['Primitive']["Density"])
-    ax[1,1].plot(results['X Coords'][1:-1], results['Primitive']["Mach"][1:-1, -1], label=labels[i])
-ax[1, 1].set_ylabel(r'Mach')
+ax[1,  0].set_ylabel(r'Pressure')
+ax[1,  1].set_ylabel(r'Energy')
 
 # Add legend only once for the figure
-fig.legend(labels, loc='upper center', ncol=2)
+fig.legend(labels, loc='upper center', ncol=3)
 
 for row in ax:
-        for col in row:
-            col.set_xlabel('x')
-            col.grid(alpha=.3)
+    for col in row:
+        for location in refinement_locations:
+            col.axvline(location, color='r', ls=':', lw=2)
+        col.set_xlabel('x')
+        col.grid(alpha=.3)
 
+fig.tight_layout()
 plt.savefig(outFolder + '/Comparison.pdf', bbox_inches='tight')
 
 
